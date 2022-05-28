c903c1de2ad5aa41dd3b4ef3ef15aa3aea376ae7
Hernan Martinez Reumann <hmreumann@hotmail.com>
2022-05-28 08:03:45 -0300

    Add Ranger BO_2

--- src/ranger/411_ranger_v2.r
+++ src/ranger/411_ranger_v2.r
@@ -29,10 +29,10 @@ dapply  <- na.roughfix( dapply )  #tambien imputo los nulos en los datos donde v
 
 #genero el modelo de Random Forest con la libreria ranger
 #notar como la suma de muchos arboles contrarresta el efecto de min.node.size=1
-param  <- list( "num.trees"=       145,  #cantidad de arboles
-                "mtry"=             8,  #cantidad de variables que evalua para hacer un split  sqrt(ncol(dtrain))
-                "min.node.size"=  379,  #tamaño minimo de las hojas
-                "max.depth"=        15   # 0 significa profundidad infinita
+param  <- list( "num.trees"=       2339,  #cantidad de arboles
+                "mtry"=             9,  #cantidad de variables que evalua para hacer un split  sqrt(ncol(dtrain))
+                "min.node.size"=  520,  #tamaño minimo de las hojas
+                "max.depth"=        24   # 0 significa profundidad infinita
               )
 
 set.seed(102199) #Establezco la semilla aleatoria
@@ -64,7 +64,7 @@ entrega  <- as.data.table( list( "numero_de_cliente"= dapply[  , numero_de_clien
 # HT  representa  Hiperparameter Tuning
 dir.create( "./labo/exp/",  showWarnings = FALSE ) 
 dir.create( "./labo/exp/KA2411/", showWarnings = FALSE )
-archivo_salida  <- "./labo/exp/KA2411/KA_411_004.csv"
+archivo_salida  <- "./labo/exp/KA2411/KA_411_008.csv"
 
 #genero el archivo para Kaggle
 fwrite( entrega, 
